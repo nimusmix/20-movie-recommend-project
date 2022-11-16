25a1c453774828e3803ff65617b158bab9e937ca
nimusmix <nimusmix@gmail.com>
2022-11-16 11:19:26 +0900

    back skeleton

--- final-pjt-back/community/models.py
+++ final-pjt-back/community/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
+from movies.models import Movie
 
 # Create your models here.
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = model.CharField(max_length=50)
