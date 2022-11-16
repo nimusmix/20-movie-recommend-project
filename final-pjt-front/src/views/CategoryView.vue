88449cbbddc8001879e97e6198521ea3fa499e94
nimusmix <nimusmix@gmail.com>
2022-11-16 23:55:12 +0900

    CategoryView

--- final-pjt-front/src/views/CategoryView.vue
+++ final-pjt-front/src/views/CategoryView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2>카테고리</h2>
     <div class="row row-cols-1 row-cols-md-4
          g-4">
       <article 
@@ -10,7 +11,6 @@
       <CategoryItem :movie="movie"/>
     </article>
     </div>
-    
   </div>
 </template>
 
