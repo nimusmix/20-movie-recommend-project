133b876a2d8c231ef87e33f698ec1b6d1d8dd0f4
heeje <heeje1996@gmail.com>
2022-11-16 22:25:19 +0900

    movie card 기능추가

--- final-pjt-front/src/components/CategoryItem.vue
+++ final-pjt-front/src/components/CategoryItem.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <h1>CategoryItem</h1>
+  <div class="card">
+    <router-link :to="{ name: 'DetailView', params: { pk: movie.id, movie:movie }}">
+      <img :src="`https://image.tmdb.org/t/p/original/${ movie.poster_path }`" 
+      class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ movie.id }}</h5>
+        <h5 class="card-title">{{ movie.title }}</h5>
+        <p class="card-text">{{ movie.vote_average }}</p>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CategoryItem',
+  props:{
+    movie:Object
+  }
 }
 </script>
 
