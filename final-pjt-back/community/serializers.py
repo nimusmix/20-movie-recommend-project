6c732db7cf5b57242d89eeba09b05a277946d7e2
heisje <heeje1996@gmail.com>
2022-11-17 11:04:25 +0900

    피드 시리얼라이저 함수기능 레퍼런스 업데이트

--- final-pjt-back/community/serializers.py
+++ final-pjt-back/community/serializers.py
@@ -1,33 +1,36 @@
 from rest_framework import serializers
 from .models import Movie, Review
+# from .serializers import MovieSerializer
 
+class MovieSerializer(serializers.ModelSerializer):
 
-# class MovieListSerializer(serializers.ModelSerializer):
-#     username = serializers.CharField(source='user.username', read_only=True)
-
-#     class Meta:
-#         model = Movie
-#         # fields = ('id', 'title', 'content')
-#         fields = ('id', 'title', 'content', 'user', 'username')
-
+    class Meta:
+        model = Movie
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
+    
+    movie_title = serializers.CharField(source='movie.title', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
+    plus_content = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
         fields = '__all__'
         read_only_fields = ('movie','user',)
+    
+    def get_plus_content(self,obj):
+        # 현재시간 - 생성시간
+	    return (obj.content + 'aaaaaaaaaaaaaaaaaaaa')
 
 
-# class MovieSerializer(serializers.ModelSerializer):
-#     review_set = ReviewSerializer(many=True, read_only=True)
-#     review_count = serializers.IntegerField(source='review_set.count', read_only=True)
-#     username = serializers.CharField(source='user.username', read_only=True)
 
-#     class Meta:
-#         model = Movie
-#         fields = '__all__'
-#         read_only_fields = ('user', )
-
-
+class ReviewCustomSerializer(serializers.ModelSerializer):
+    movie_title = serializers.CharField(source='movie.title', read_only=True)
+    username = serializers.CharField(source='user.username', read_only=True)
+    class Meta:
+        model = Review
+        fields = '__all__'
+        read_only_fields = ('user', )
+        #read_only_fields = ('movie','user','movie_set','movie_score')
 
