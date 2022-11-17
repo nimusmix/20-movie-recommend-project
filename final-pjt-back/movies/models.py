111ddde64c174a022534a3abff873eeeff0cb0f9
nimusmix <nimusmix@gmail.com>
2022-11-17 11:24:29 +0900

    add ott model

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
+class Ott(models.Model):
+    name = models.CharField(max_length=50)
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     genres = models.ManyToManyField(Genre, related_name='movies')
+    otts = models.ManyToManyField(Ott, related_name='movies')
     overview = models.TextField()
     release_date = models.DateField()
     popularity = models.FloatField()
@@ -17,7 +21,3 @@ class Movie(models.Model):
     poster_path = models.CharField(max_length=200)
     original_language = models.CharField(max_length=100)
     original_title = models.CharField(max_length=100)
-    netflix = models.BooleanField()
-    watcha = models.BooleanField()
-    wavve = models.BooleanField()
-    disney = models.BooleanField()
